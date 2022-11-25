172590500ff18bda9622b8f4d63384ce05774bdf
Aleksandr <soft@vhlv.ru>
2022-11-25 10:39:25 +0300

    feat: add open/close mobile menu

--- src/index.js
+++ src/index.js
@@ -5,3 +5,28 @@ function requireAll(icon) {
 }
 
 requireAll(require.context('../src/svg', true, /\.svg$/));
+
+const menu = document.querySelector('.header__menu');
+const menuButton = document.querySelector('.header__menu-button');
+const menuOverlay = document.querySelector('.overlay');
+const cross = menuButton.querySelector('.cross');
+const toggleMenuButton = () => {
+  menu.classList.toggle('header__menu_opened');
+  document.body.classList.toggle('scroll-lock');
+  menuOverlay.classList.toggle('overlay_active');
+  cross.classList.toggle('cross_active');
+};
+
+const closeMenu = (e) => {
+  if (e.target.classList.contains('header__menu-link') || e.target.classList.contains('overlay')) {
+    menuButton.classList.remove('header__menu-button_opened');
+    menu.classList.remove('header__menu_opened');
+    menuOverlay.classList.remove('overlay_active');
+    cross.classList.remove('cross_active');
+    document.body.classList.remove('scroll-lock');
+  }
+};
+
+menuButton.addEventListener('click', toggleMenuButton);
+menu.addEventListener('click', closeMenu);
+menuOverlay.addEventListener('click', closeMenu);
